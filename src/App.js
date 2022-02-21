32ff4d037a32b8e24d2a1ff5bc2dccbed815917b
张海玉 <2030564240@qq.com>
2022-02-21 09:50:53 +0800

    feat:在React中使用路由功能

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import Header from "./components/header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <>
         <Header />
+        <Router>
+          {/* TODO: Router下必须只有一个根元素，所以需要Routes包裹起来 */}
+          <Routes>
+            <Route path='/about' element={<div>about</div>}></Route>
+            <Route path='/users' element={<div>users</div>}></Route>
+            <Route path='/' element={<div>home</div>}></Route>
+          </Routes>
+        </Router>
       </>
     );
   }
